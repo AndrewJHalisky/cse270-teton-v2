991b86de3f33ebd1845a1bc3326e305a0f228817
Andrew Halisky <ajhalisky@gmail.com>
2025-08-14 16:47:44 -0400

    Verified changes

--- cse270-v16/tests/test_smokeTest.py
+++ cse270-v16/tests/test_smokeTest.py
@@ -11,7 +11,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.chrome.options import Options
 
-class TestDefaultSuite():
+class TestSmokeTest():
   def setup_method(self, method):
     options = Options()
     options.add_argument("--headless=new")
@@ -21,22 +21,33 @@ class TestDefaultSuite():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_homeTest(self):
+  def test_homePage(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/index.html")
-    self.driver.set_window_size(827, 1176)
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-logo img")
     assert len(elements) > 0
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".header-title > h1")
-    assert len(elements) > 0
     assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h1").text == "Teton Idaho"
+    assert self.driver.find_element(By.CSS_SELECTOR, ".header-title > h2").text == "Chamber of Commerce"
     assert self.driver.title == "Teton Idaho CoC"
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1 > .centered-image")
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight1")
     assert len(elements) > 0
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".spotlight2 > .centered-image")
     assert len(elements) > 0
-    elements = self.driver.find_elements(By.LINK_TEXT, "Join Us!")
+    elements = self.driver.find_elements(By.LINK_TEXT, "Join Us")
     assert len(elements) > 0
-    self.driver.find_element(By.LINK_TEXT, "Join Us!").click()
+    self.driver.find_element(By.LINK_TEXT, "Join Us").click()
+    self.driver.set_window_size(796, 1173)
+  
+  def test_adminTest(self):
+    self.driver.get("http://127.0.0.1:5500/teton/1.6/admin.html")
+    self.driver.set_window_size(796, 1172)
+    elements = self.driver.find_elements(By.ID, "username")
+    assert len(elements) > 0
+    self.driver.find_element(By.ID, "username").send_keys("augustzone")
+    self.driver.find_element(By.ID, "password").click()
+    self.driver.find_element(By.ID, "password").send_keys("asdfasdf")
+    self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
+    elements = self.driver.find_elements(By.CSS_SELECTOR, ".errorMessage")
+    assert len(elements) > 0
   
   def test_directoryTest(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/directory.html")
@@ -48,24 +59,10 @@ class TestDefaultSuite():
     elements = self.driver.find_elements(By.CSS_SELECTOR, ".gold-member:nth-child(9) > p:nth-child(2)")
     assert len(elements) > 0
   
-  def test_adminTest(self):
-    self.driver.get("http://127.0.0.1:5500/teton/1.6/admin.html")
-    self.driver.set_window_size(827, 1176)
-    elements = self.driver.find_elements(By.ID, "username")
-    assert len(elements) > 0
-    self.driver.find_element(By.ID, "password").click()
-    self.driver.find_element(By.ID, "password").send_keys("asdfasdf")
-    self.driver.find_element(By.ID, "username").click()
-    self.driver.find_element(By.ID, "username").send_keys("augustwreck")
-    self.driver.find_element(By.ID, "password").click()
-    self.driver.find_element(By.CSS_SELECTOR, "fieldset").click()
-    self.driver.find_element(By.CSS_SELECTOR, ".mysubmit:nth-child(4)").click()
-    elements = self.driver.find_elements(By.CSS_SELECTOR, ".errorMessage")
-    assert len(elements) > 0
-  
   def test_joinTest(self):
     self.driver.get("http://127.0.0.1:5500/teton/1.6/join.html")
-    self.driver.set_window_size(827, 1176)
+    self.driver.set_window_size(796, 1173)
+    self.driver.find_element(By.NAME, "fname").click()
     elements = self.driver.find_elements(By.NAME, "fname")
     assert len(elements) > 0
     self.driver.find_element(By.NAME, "fname").send_keys("Andrew")
@@ -73,10 +70,9 @@ class TestDefaultSuite():
     self.driver.find_element(By.NAME, "lname").send_keys("Halisky")
     self.driver.find_element(By.NAME, "bizname").click()
     self.driver.find_element(By.NAME, "bizname").send_keys("Big Mans")
-    self.driver.find_element(By.NAME, "biztitle").click()
+    self.driver.find_element(By.CSS_SELECTOR, ".myinput:nth-child(5)").click()
     self.driver.find_element(By.NAME, "biztitle").send_keys("Developer")
     self.driver.find_element(By.NAME, "submit").click()
-    self.driver.find_element(By.NAME, "email").click()
     elements = self.driver.find_elements(By.NAME, "email")
     assert len(elements) > 0
     self.driver.find_element(By.NAME, "email").send_keys("ajhalisky@gmail.com")
@@ -88,6 +84,8 @@ class TestDefaultSuite():
     self.driver.find_element(By.NAME, "websiteURL").click()
     self.driver.find_element(By.NAME, "websiteURL").send_keys("https://bigmans.org")
     self.driver.find_element(By.CSS_SELECTOR, "textarea").click()
-    self.driver.find_element(By.CSS_SELECTOR, "textarea").send_keys("Big Man\'s is a nonprofit organization.")
+    self.driver.find_element(By.CSS_SELECTOR, "textarea").send_keys("Big Mans is a nonprofit organization.")
     self.driver.find_element(By.NAME, "submit").click()
+    elements = self.driver.find_elements(By.CSS_SELECTOR, "tr:nth-child(3) > .left-justify")
+    assert len(elements) > 0
   
